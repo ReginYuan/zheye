440c4dd569f628cd9412dcc4f0b6af278be030fb
爱做梦的袁大事 <oncwnuARQZexvg5c0mK5Lmzmt0uY@git.weixin.qq.com>
2020-12-22 16:12:49 +0800

    4-5 ColumnList 组件属性分析和组件编码

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,12 +1,12 @@
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
+  <div>
+    <div v-for="column in list" :key="column.id">
       <img :src="column.avatar" :alt="column.title" />
       <h5>{{column.title}}</h5>
       <p>{{column.description}}</p>
       <a href="#">进入专栏</a>
-    </li>
-  </ul>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
